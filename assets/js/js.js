a1999f68a4d824b265b127af8e3e7f488316b905
Anh Vuong <vta19797@gmail.com>
2022-03-06 18:08:46 -0600

    handle form submission

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,4 +1,4 @@
-var buttonEl=document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //var taskItemEl = document.createElement("li");
@@ -6,10 +6,12 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 //tasksToDoEl.appendChild(taskItemEl);
 //taskItemEl.className="task-item";
 
-var createTaskHandler = function (){
+var createTaskHandler = function (event){
+    event.preventDefault();
+
     var listItemEl=document.createElement("li");
     listItemEl.className="task-item";
     listItemEl.textContent="This is a new task";
     tasksToDoEl.appendChild(listItemEl);
 }
-buttonEl.addEventListener("click",createTaskHandler);
+formEl.addEventListener("submit",createTaskHandler);
