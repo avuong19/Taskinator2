0d3b31b9a1d828a76880b02f33ff0ea98c96d4e6
Anh Vuong <vta19797@gmail.com>
2022-03-06 22:25:49 -0600

    add the ability to edit task

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,5 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 //var taskItemEl = document.createElement("li");
 //taskItemEl.textContent = "hello";
@@ -30,6 +32,8 @@ var taskFormHandler = function (event){
 var createTaskEl= function(taskDataObj){
     var listItemEl=document.createElement("li");
     listItemEl.className="task-item";
+
+    listItemEl.setAttribute("data-task-id",taskIdCounter);
     
     
     var taskInfoEl=document.createElement("div");
@@ -37,11 +41,100 @@ var createTaskEl= function(taskDataObj){
     taskInfoEl.innerHTML="<h3 class='task-name'>"+ taskDataObj.name+"</h3><span class='task-type'>" + taskDataObj.type+"</span>";
 
     listItemEl.appendChild(taskInfoEl);
+
+    var taskActionsEl= createTaskActions(taskIdCounter);
+    console.log(taskActionsEl);
+
+    listItemEl.appendChild(taskActionsEl);
     tasksToDoEl.appendChild(listItemEl);
+
+    taskIdCounter++;
+
+   
+    
 }
+
+var createTaskActions = function (taskId){
+    var actionContainerEl=document.createElement("div");
+    actionContainerEl.className="task-actions";
+    // create edit button
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(editButtonEl);
+
+    // create delete button
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(deleteButtonEl);
+
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(statusSelectEl);
+
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+    for (var i = 0; i < statusChoices.length; i++) {
+        // create option element
+        var statusOptionEl = document.createElement("option");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value", statusChoices[i]);
+      
+        // append to select
+        statusSelectEl.appendChild(statusOptionEl);
+      }
+    return actionContainerEl;
+}
+
+var tasksButtonHandler =function(event){
+    var targetEl=event.target;
+
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+      } 
+
+
+    if(event.target.matches(".delete-btn")){
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+}
+
+var deleteTask = function(taskId){
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+}
+
+var editTask = function(taskId) {
+    console.log("editing task #" + taskId);
+  
+    // get task list item element
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // get content from task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    document.querySelector("input[name='task-name']").value = taskName;
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    document.querySelector("select[name='task-type']").value = taskType;
+
+    formEl.setAttribute("data-task-id", taskId);
+
+    document.querySelector("#save-task").textContent = "Save Task";
+    
+  }
 
 
 
 
 formEl.addEventListener("submit",taskFormHandler);
 formEl.reset();
+pageContentEl.addEventListener("click",tasksButtonHandler);
